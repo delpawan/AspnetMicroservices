5e91c05c6edf087a27d5bdd32dea24ef902279db
Pawan Rai <pawan.rai@publicissapient.com>
2021-05-19 19:42:26 +0530

    Catalog.API project added and Docker compose

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private readonly MongoClient _mongoClient;
+        private readonly IConfiguration _configuration;
+        public CatalogContext(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            var connectionString = _configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+            _mongoClient = new MongoClient(connectionString);
+            var databaseName = _configuration.GetValue<string>("DatabaseSettings:DatabaseName");
+            var database = _mongoClient.GetDatabase(databaseName);
+
+            Products = database.GetCollection<Product>(_configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            CatalogContextSeed.SeedData(Products);            
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,91 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _catalogContext;
+
+        public ProductRepository(ICatalogContext catalogContext)
+        {
+            _catalogContext = catalogContext;
+        }
+
+        public async Task CreateProduct(Product product)
+        {
+            await _catalogContext
+                .Products
+                .InsertOneAsync(product);
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            FilterDefinition<Product> filter = Builders<Product>
+                .Filter.Eq(p => p.Id, id);
+
+            var result = await _catalogContext
+                 .Products
+                 .DeleteOneAsync(filter);
+
+            return result.IsAcknowledged 
+                && result.DeletedCount > 0;
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            FilterDefinition<Product> filter = Builders<Product>
+                .Filter
+                .Eq(p => p.Id, id);
+
+            return await _catalogContext
+                .Products
+                .Find(filter)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategoryName(string categoryName)
+        {
+            FilterDefinition<Product> filter = Builders<Product>
+                .Filter.Eq(p => p.Category, categoryName);
+
+            return await _catalogContext
+                .Products
+                .Find(filter)
+                .ToListAsync();            
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>
+                .Filter.Eq(p => p.Name, name);
+
+            return await _catalogContext
+                .Products
+                .Find(filter)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _catalogContext
+                .Products
+                .Find(p => true)
+                .ToListAsync();                
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var result = await _catalogContext
+                .Products
+                .ReplaceOneAsync(p => p.Id.Equals(product.Id), product);
+
+            return result.IsAcknowledged && result.ModifiedCount > 0;
+        }
+    }
+}
